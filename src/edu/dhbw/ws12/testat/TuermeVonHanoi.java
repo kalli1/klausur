0cf93b72a4627a4263415a306301bd53f3a4cfb4
kalli1 <kalli_1@hotmail.de>
2013-04-20 17:25:14 +0200

    Added Test Output

--- src/edu/dhbw/ws12/testat/TuermeVonHanoi.java
+++ src/edu/dhbw/ws12/testat/TuermeVonHanoi.java
@@ -24,6 +24,7 @@ public class TuermeVonHanoi {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hallo, test");
 		//showTowers();
 		zstStart = System.currentTimeMillis();
 		
